ee76d0caf4fe6a66d7c068bde5b950ed434020b4
Artjoms Dimitrijevs <kukurechka174@gmail.com>
2025-05-25 00:18:30 +0300

    .

--- app/src/main/java/com/example/gifsearch/data/remote/GiphyAPIService.kt
+++ app/src/main/java/com/example/gifsearch/data/remote/GiphyAPIService.kt
@@ -4,3 +4,11 @@ import com.example.gifsearch.data.models.GiphyResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+interface GiphyAPIService {
+    @GET("v1/gifs/trending")
+    suspend fun getTrendingsGifs(
+        @Query("api_key") apiKey: String,
+        @Query("limit") limit: Int,
+        @Query("offset") offset: Int
+    ): GiphyResponse
+}
